fa239f6dd8352f1649c47fecce00ca4a824c4c33
RyanMdg <ryandeguia.dev@gmail.com>
2024-09-29 02:51:06 +0800

    testing

--- Front-end/App.js
+++ Front-end/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello world!</Text>
+      <Text>This is you flow!</Text>
       <StatusBar style="auto" />
     </View>
   );
