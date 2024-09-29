2608f5a08d5bb23ed313a74f770f7239f872b042
RyanMdg <ryandeguia.dev@gmail.com>
2024-09-30 00:57:07 +0800

    test

--- Front-end/App.js
+++ Front-end/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
+      <Text>hello world! welcome to u flow! this is what it is!</Text>
       <Text>This is you flow!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
